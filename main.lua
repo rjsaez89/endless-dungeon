7101dbe09c0f3bfbc48f822609a68da0973b3ad9
Reyshawn <rjsaez89@harrisonburg.k12.va.us>
2019-10-04 14:26:35 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,40 @@
 local Map = require 'core/map'
 
 function love.load()
+    map = Map:new(5, 5)
   x = 100
   y = 500
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
+
+  floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_9.png')
+--altar     = love.graphics.newImage('altar-asset.png')
+template = { --a 3 x 3 map with the altar texture in the middle
+             {floorTile, floorTile, floorTile},
+             {floorTile, altar, floorTile},
+             {floorTile, floorTile, floorTile},
+           }
+map = Map:new(template)
+
 end
 
 function love.update(dt)
-  -- Nothing to update yet
+  if love.keyboard.isDown('right') then
+    x = x + 1
+  end
+  if love.keyboard.isDown('left') then
+    x = x - 1
+  end
+  if love.keyboard.isDown('right') then
+    y = y + 1
+  end
+  if love.keyboard.isDown('right') then
+    y = y - 1
+  end
+
 end
 
 function love.draw()
+    map:draw()
   love.graphics.print('wasupp, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
