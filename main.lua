c853b5f412801ae9b329c718c7df92a8ef0fd1a1
Reyshawn <rjsaez89@harrisonburg.k12.va.us>
2019-10-17 13:49:38 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,14 +5,38 @@ function love.load()
   y = 500
   playerImg = love.graphics.newImage('assets-1/player/base/octopode_1.png')
 
-  floorTile = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_9.png')
+  floorTile = love.graphics.newImage('assets-1/dungeon/floor/cage_0.png')
+  cobalt = love.graphics.newImage('assets-1/dungeon/floor/black_cobalt_9.png')
 --altar     = love.graphics.newImage('altar-asset.png')
 template = { --a 3 x 3 map with the altar texture in the middle
-             {floorTile, floorTile, floorTile},
-             {floorTile, altar, floorTile},
-             {floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, altar, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
            }
+
+
 map = Map:new(template)
+
+walls = { --a 3 x 3 map with the altar texture in the middle
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, 'nil', floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+             {floorTile, floorTile, floorTile, floorTile, floorTile, floorTile},
+           }
+
+
+wallMap = Map:new(walls)
 
 end
 
@@ -23,10 +47,10 @@ function love.update(dt)
   if love.keyboard.isDown('left') then
     x = x - 1
   end
-  if love.keyboard.isDown('right') then
+  if love.keyboard.isDown('down') then
     y = y + 1
   end
-  if love.keyboard.isDown('right') then
+  if love.keyboard.isDown('up') then
     y = y - 1
   end
 
@@ -34,6 +58,7 @@ end
 
 function love.draw()
     map:draw()
+    wallMap:draw()
   love.graphics.print('wasupp, world!', 0, 0)
   love.graphics.draw(playerImg, x, y)
 end
